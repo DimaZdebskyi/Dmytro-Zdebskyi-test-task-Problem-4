c7132b0cb77d8218fbeac6b855db2260e4d27fc3
DimaZdebskyi <dimazdebskui22@gmail.com>
2019-06-02 18:19:18 +0300

    version 2.0

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const states = ['Choose..', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 const mainForm = document.getElementById('main-form');
 const cancelBtn = document.getElementsByClassName('cancel-btn')[0];
 const submitBtn = document.getElementsByClassName('submit-btn')[0];
@@ -5,112 +6,101 @@ const selectStates = document.getElementsByClassName('states')[0];
 const inputs = document.getElementsByTagName('input');
 
 let valid = true;
-k
+
 cancelBtn.onclick = () => clear();
-submitBtn.onclick = () => { qwe(); finish() };
+submitBtn.onclick = () => { inputFields(); finish(); };
 
 function clear() {
-    for (let i = 0; i < inputs.length; i++) {
-        inputs[i].value = '';
-    }
-    selectStates.value = 'Choose..';
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].value = '';
+  }
+  selectStates.value = 'Choose..';
 }
 
-function qwe() {
-    const name = mainForm.name;
-    const ads1 = mainForm.ads1;
-    const ads2 = mainForm.ads2;
-    const city = mainForm.city;
-    const zip = mainForm.zip;
-    const state = mainForm.state;
+function inputFields() {
+  const name = mainForm.name;
+  const ads1 = mainForm.ads1;
+  const ads2 = mainForm.ads2;
+  const city = mainForm.city;
+  const zip = mainForm.zip;
+  const state = mainForm.state;
 
-    const arr = [name, ads1, ads2, city, zip, state];
+  const inputFieldsArray = [name, ads1, ads2, city, zip, state];
 
-    verifyInputs(arr);
+  verifyInputs(inputFieldsArray);
 }
 
 function verifyInputs(formInputs) {
-    formInputs.forEach((el) => {
-        switch(el.name) {
-            case 'name':
-                isEmpty(el);
-                break;
-            case 'ads1':
-                isEmpty(el);
-                break; 
-            case 'city':
-                isEmpty(el);
-                break;
-            case 'state':
-                isNone(el);
-                break;
-            case 'zip':
-                ifLengthFive(el);
-                break;
-        }
-    });
+  formInputs.forEach((el) => {
+    switch(el.name) {
+      case 'name':
+        isEmpty(el);
+        break;
+      case 'ads1':
+        isEmpty(el);
+        break; 
+      case 'city':
+        isEmpty(el);
+        break;
+      case 'state':
+        isNone(el);
+        break;
+      case 'zip':
+        ifLengthFive(el);
+        break;
+      default:
+        break;
+    }
+  });
 }
 
 function error(input) {
-    alert(`Enter valid ${input.name}`);
-    valid = false;
+  alert(`Enter valid ${input.name}`);
+  valid = false;
 }
 
 function isEmpty(input) {
-    if (!input.value.length) {
-        error(input);
-    }
+  if (!input.value.length) {
+    error(input);
+  }
 }
 
 function ifLengthFive(input) {
-    if (input.value.length !== 5) {
-        error(input);
-    }
+  if (input.value.length !== 5) {
+    error(input);
+  }
 }
 
 function isNone(input) {
-    if (input.value === 'Choose..') {
-        error(input);
-    }
+  if (input.value === 'Choose..') {
+    error(input);
+  }
 }
 
 function finish() {
-    if(valid) { 
-        alert('Regestration finish')
-        clear()
-    }  
-    valid = true
-}
-function isNumber(event) {
-    let keycode = event.keyCode;
-    if (keycode > 48 && keycode < 57) {
-        return true;
-    }
-    return false;
+  if (valid) {
+    alert('Regestration finish');
+    clear();
+  }
+  valid = true;
 }
 
-function isAlpha(event) {
-    let keycode = event.keyCode;
-    if ((keycode > 64 && keycode < 122) || keycode == 39) {
-        return true;
-    }
-    return false;
+function isNumber({keyCode}) {
+  return keyCode > 48 && keyCode < 57;
 }
 
-function isAlphanumeric(event) {
-    let keycode = event.keyCode;
-    if ((keycode > 64 && keycode < 122) || keycode == 39 || (keycode > 48 && keycode < 57)) {
-        return true;
-    }
-    return false;
+function isAlpha({keyCode}) {
+  return (keyCode > 64 && keyCode < 122) || keyCode == 39;
+}
+
+function isAlphanumeric({keyCode}) {
+  return (keyCode > 64 && keyCode < 122) || keyCode == 39 || (keyCode > 48 && keyCode < 57);
 }
 
 function addStateName() {
-    states.forEach((elName) => {
-        selectStates.innerHTML += `<option value=${elName}>${elName}</option>`;
-    });
+  states.forEach((elName) => {
+    selectStates.innerHTML += `<option value="${elName}">${elName}</option>`;
+  });
 }
-
-const states = ['Choose..',  'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
 addStateName();
